0607862d68196fb4ec1db81f4884a86427b4a7e5
Douglas Moura Silva <53541088+douglasmoura1993@users.noreply.github.com>
2020-01-25 16:42:40 -0300

    Add files via upload

---
+++ exercicio01.py
@@ -0,0 +1,5 @@
+nome = input('Digite o seu nome: ')
+print('Olá ',nome,', Seja bem vindo ao meu primeiro código de python!')
+print('Estou feliz em saber que você gastou um pouco do seu tempo para '
+      'poder avaliar o que eu venho'
+      ' aprendendo com esse novo curso ;) ')
---
+++ primeiroScript.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome: ')
+idade = input ('Qual é a sua idade: ')
+peso = input('Digite o seu peso: ')
+print ('Olá ',nome,'você disse que tem ',idade,'correto?')
+print ('E você tem',peso,' kg')
