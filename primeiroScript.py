53d9a35dce42fdfefa86167ef9b0a329901ff717
Douglas Moura Silva <53541088+douglasmoura1993@users.noreply.github.com>
2022-08-18 14:25:15 -0300

    Delete primeiroScript.py

--- primeiroScript.py
+++
@@ -1,5 +0,0 @@
-nome = input('Qual é o seu nome: ')
-idade = input ('Qual é a sua idade: ')
-peso = input('Digite o seu peso: ')
-print ('Olá ',nome,'você disse que tem ',idade,'correto?')
-print ('E você tem',peso,' kg')
