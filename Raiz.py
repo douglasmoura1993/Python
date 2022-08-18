e8c1b8615c019259d5d6b98bedcdadfa49620d85
Douglas Moura Silva <53541088+douglasmoura1993@users.noreply.github.com>
2022-08-18 14:25:50 -0300

    Delete Raiz.py

--- Raiz.py
+++
@@ -1,5 +0,0 @@
-import math
-n = float(input('Digite um numero: '))
-print('O dobro de {} é {}'.format(n, (n * 2)))
-print('O Triplo de {} é {}'.format(n, (n * 3)))
-print('A Raíz de {} é {}'.format(n, (math.sqrt(n))))
